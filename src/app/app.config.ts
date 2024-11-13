252a423f8800fe69d3ba0fe1bdc6345ee9dc9b0b
ManuvidyaS <manuvidyasmd@gmail.com>
2024-11-13 09:29:13 +0530

    added

---
+++ src/app/app.config.ts
@@ -0,0 +1,11 @@
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideHttpClient(),provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), importProvidersFrom(MatSnackBarModule), provideAnimationsAsync()]
+};
